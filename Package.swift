7df22398633946a083dbf8d424ca8b5e2315382d
donaldfilimon <cbkshadow@icloud.com>
2024-11-27 18:51:45 -0500

    init

---
+++ Package.swift
@@ -0,0 +1,13 @@
+// swift-tools-version: 6.0
+
+import PackageDescription
+
+let package = Package(
+    name: "gama",
+    targets: [
+        .executableTarget(
+            name: "gama",
+            dependencies: []
+        )
+    ]
+)
